c84740849fb960c3a468967c2863760086093d30
Nedim Mujezinovic <mujezinovic@gmail.com>
2023-09-19 12:12:50 +0200

    Add KristofferStrube.Blazor.SVGEditor

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SvgEditorMudBlazor;
 using MudBlazor.Services;
+using KristofferStrube.Blazor.SVGEditor.Extensions;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,5 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
-
+builder.Services.AddSVGEditor();
 await builder.Build().RunAsync();
